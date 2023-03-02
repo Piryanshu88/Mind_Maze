20a42d6a280f5d2b94e3f973a7ad3264abec64d8
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-02 19:25:37 +0530

    adding routes for user by id

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -87,6 +87,22 @@ userRouter.get("/all-users", async (req, res) => {
   }
 });
 
+userRouter.get("/all-users/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const users = await UserModel.find({ _id: id });
+      res.status(201).json({
+        status: "success",
+        data: users,
+      });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: "Something went wrong", status: "error" });
+    }
+  });
+
 module.exports = {
   userRouter,
 };

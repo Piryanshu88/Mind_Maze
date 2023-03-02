ca2a52c99d7789ed404f41b01319fc5b39f31eb7
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-02 19:24:22 +0530

    adding register and login routes

--- server/index.js
+++ server/index.js
@@ -3,25 +3,29 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const { connection } = require("./config/db");
+const { userRouter } = require("./routes/user.routes");
 
 const app = express();
 
-app.use(cors({
-    origin:"*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Home Page")
-})
+  res.send("Home Page");
+});
 
-app.listen(8080,async()=>{
-    try {
-        await connection
-        console.log(`server running at port ${process.env.port}`);
-    } catch (error) {
-        console.log(error);
-    }
-    
-})
+app.use("/user", userRouter);
+
+app.listen(8080, async () => {
+  try {
+    await connection;
+    console.log(`server running at port ${process.env.port}`);
+  } catch (error) {
+    console.log(error);
+  }
+});

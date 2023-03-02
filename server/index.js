24232eb43182311354b567c2397629754e3a0ede
Saurav903 <sk8517167@gmail.com>
2023-03-02 18:49:40 +0530

    env send

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
     res.send("Home Page")
 })
 
-app.listen(process.env.port,async()=>{
+app.listen(8080,async()=>{
     try {
         await connection
         console.log(`server running at port ${process.env.port}`);

9c861c6c116f103f7b641725ef7350442d850665
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-03 14:13:51 +0530

    Adding reducer

--- client/src/Admin/Redux/reducer.ts
+++ client/src/Admin/Redux/reducer.ts
@@ -1,3 +1,4 @@
+import * as types from "./actionTypes";
 const initialState = {
   loading: false,
   isLoggedIn: false,
@@ -7,11 +8,28 @@ const initialState = {
 
 interface Action {
   type: string;
-  payload: any;
+  payload?: object;
 }
 
 const reducer = (state = initialState, { type, payload }: Action) => {
   switch (type) {
+    case types.GET_DATA_REQ:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.GET_DATA_SUC:
+      return {
+        ...state,
+        loading: false,
+        user: payload.data,
+      };
+    case types.GET_DATA_ERR:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
     default:
       return state;
   }

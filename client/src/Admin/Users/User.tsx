e65176252edc30b1af55def8a48a456038461d47
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-03 19:52:33 +0530

    adding responsiveness to user

--- client/src/Admin/Users/User.tsx
+++ client/src/Admin/Users/User.tsx
@@ -1,16 +1,27 @@
-import { Text } from "@chakra-ui/react";
+import { IconButton, Skeleton, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDataUser, useDataSuc } from "../Redux/action";
 import { Payload, State, users } from "../Redux/reducer";
 import styles from "./User.module.css";
 import { RiUser3Line } from "react-icons/ri";
+import { DeleteIcon } from "@chakra-ui/icons";
+import axios from "axios";
 interface Store {
   adminReducer: State;
 }
 export const User = () => {
   const dispatch = useDispatch();
   const [userdata, setData] = useState<users[]>([]);
+  const handleClick = (id: string) => {
+    return axios
+      .delete(`https://lazy-tan-shrimp-tux.cyclic.app/user/delete/${id}`)
+      .then(() =>
+        getDataUser().then((re: Payload) =>
+          setData(re.data.sort((a, b) => b.points - a.points))
+        )
+      );
+  };
   useEffect(() => {
     getDataUser().then((re: Payload) =>
       setData(re.data.sort((a, b) => b.points - a.points))
@@ -34,6 +45,9 @@ export const User = () => {
         <div className={styles.user_box_headings}>
           <Text>Total Score</Text>
         </div>
+        <div className={styles.user_box_headings}>
+          <Text>Delete</Text>
+        </div>
       </div>
       {userdata?.map((el, i) => {
         return (
@@ -50,6 +64,18 @@ export const User = () => {
             </div>
             <div className={styles.user_box_text}>
               <Text>{el.points} pts. </Text>
+            </div>
+            <div>
+              <IconButton
+                onClick={() => handleClick(el._id)}
+                variant="outline"
+                colorScheme="red"
+                color={"#FF0080"}
+                border="none"
+                _hover={{ color: "#fff" }}
+                aria-label="Send email"
+                icon={<DeleteIcon />}
+              />
             </div>
           </div>
         );

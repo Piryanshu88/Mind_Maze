7033574bfd466df4935d497d39df28eccefa4b93
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-04 15:27:11 +0530

    changing margin for admin

--- client/src/Admin/Admin.tsx
+++ client/src/Admin/Admin.tsx
@@ -28,55 +28,57 @@ export const Admin = () => {
   //     AOS.init({ delay: 500 });
   //   }, []);
   return (
-    <div className={styles.admin_box}>
-      <div className={styles.admin_box_left}>
-        <Flex
-          onClick={() => setActiveTab("users")}
-          boxShadow={
-            activeTab == "users"
-              ? "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
-              : "rgba(0, 0, 0, 0.1) 0px 4px 12px"
-          }
-        >
-          <FiUsers />
-          <Text>Users</Text>
-        </Flex>
-        <Flex
-          onClick={() => setActiveTab("questions")}
-          boxShadow={
-            activeTab == "questions"
-              ? "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
-              : "rgba(0, 0, 0, 0.1) 0px 4px 12px"
-          }
-        >
-          <BiBook />
-          <Text>All Questions</Text>
-        </Flex>
-        <Flex
-          onClick={() => setActiveTab("addquestions")}
-          boxShadow={
-            activeTab == "addquestions"
-              ? "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
-              : "rgba(0, 0, 0, 0.1) 0px 4px 12px"
-          }
-        >
-          <BiBookAdd />
-          <Text>Add Question</Text>
-        </Flex>
-        <Flex
-          onClick={() => setActiveTab("about")}
-          boxShadow={
-            activeTab == "about"
-              ? "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
-              : "rgba(0, 0, 0, 0.1) 0px 4px 12px"
-          }
-        >
-          <AiOutlineInfoCircle />
-          <Text>About</Text>
-        </Flex>
-      </div>
-      <div className={styles.admin_box_right}>
-        {getCorrectScreen(activeTab)}
+    <div className={styles.admin}>
+      <div className={styles.admin_box}>
+        <div className={styles.admin_box_left}>
+          <Flex
+            onClick={() => setActiveTab("users")}
+            boxShadow={
+              activeTab == "users"
+                ? "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
+                : "rgba(0, 0, 0, 0.1) 0px 4px 12px"
+            }
+          >
+            <FiUsers />
+            <Text>Users</Text>
+          </Flex>
+          <Flex
+            onClick={() => setActiveTab("questions")}
+            boxShadow={
+              activeTab == "questions"
+                ? "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
+                : "rgba(0, 0, 0, 0.1) 0px 4px 12px"
+            }
+          >
+            <BiBook />
+            <Text>All Questions</Text>
+          </Flex>
+          <Flex
+            onClick={() => setActiveTab("addquestions")}
+            boxShadow={
+              activeTab == "addquestions"
+                ? "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
+                : "rgba(0, 0, 0, 0.1) 0px 4px 12px"
+            }
+          >
+            <BiBookAdd />
+            <Text>Add Question</Text>
+          </Flex>
+          <Flex
+            onClick={() => setActiveTab("about")}
+            boxShadow={
+              activeTab == "about"
+                ? "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
+                : "rgba(0, 0, 0, 0.1) 0px 4px 12px"
+            }
+          >
+            <AiOutlineInfoCircle />
+            <Text>About</Text>
+          </Flex>
+        </div>
+        <div className={styles.admin_box_right}>
+          {getCorrectScreen(activeTab)}
+        </div>
       </div>
     </div>
   );
